15053e10d83d56889644d3d2e03c856c03ceed01
krp97 <k.plata576@gmail.com>
2021-04-29 22:03:26 +0200

    Adjust gyro and accelerometer data serialization

--- app/src/main/java/pl/ppiwd/exerciseanalyst/activities/utils/SessionSerializer.java
+++ app/src/main/java/pl/ppiwd/exerciseanalyst/activities/utils/SessionSerializer.java
@@ -1,38 +1,68 @@
 package pl.ppiwd.exerciseanalyst.activities.utils;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
 
+import java.util.Map;
+import java.util.Set;
+
+import pl.ppiwd.exerciseanalyst.persistence.entities.AccelerometerMeasEntity;
+import pl.ppiwd.exerciseanalyst.persistence.entities.GyroscopeMeasEntity;
 import pl.ppiwd.exerciseanalyst.persistence.entities.SessionWithMeasurements;
+import pl.ppiwd.exerciseanalyst.utils.Vector3d;
 
 public class SessionSerializer {
-    private ExclusionStrategy excludedParams;
     private SessionWithMeasurements sessionData;
     private Gson gson;
 
     public SessionSerializer(SessionWithMeasurements sessionData) {
         this.sessionData = sessionData;
-        this.excludedParams = new ExclusionStrategy() {
-            @Override
-            public boolean shouldSkipField(FieldAttributes field) {
-                if (field.getDeclaringClass() != SessionWithMeasurements.class
-                        && (field.getName().equals("sessionId") || field.getName().equals("measurementId"))) {
-                    return true;
-                }
-                return false;
-            }
 
-            @Override
-            public boolean shouldSkipClass(Class<?> clazz) {
-                return false;
-            }
+        final JsonSerializer<GyroscopeMeasEntity> gyroEntitySerializer = (measurement, typeOfSrc, context) -> {
+            JsonObject object = new JsonObject();
+            object.addProperty("timestampUtc", measurement.getTimestampUtc());
+            JsonElement vectorSerialized = context.serialize(measurement.getVector(), Vector3d.class);
+            return addToParent(object, vectorSerialized.getAsJsonObject());
         };
-        this.gson = new GsonBuilder().addSerializationExclusionStrategy(this.excludedParams).create();
+
+        final JsonSerializer<AccelerometerMeasEntity> accelEntitySerializer = (measurement, typeOfSrc, context) -> {
+            JsonObject object = new JsonObject();
+            object.addProperty("timestampUtc", measurement.getTimestampUtc());
+            JsonElement vectorSerialized = context.serialize(measurement.getVector());
+            return addToParent(object, vectorSerialized.getAsJsonObject());
+        };
+
+        final JsonSerializer<SessionWithMeasurements> sessionEntitySerializer = (session, typeOfSrc, context) -> {
+            JsonObject object = new JsonObject();
+            JsonElement sessionSerialized = context.serialize(session.getSessionEntity());
+            addToParent(object, sessionSerialized.getAsJsonObject());
+
+            object.add("accelerometerMeasEntities", context.serialize(session.getAccelerometerMeasEntities()));
+            object.add("gyroscopeMeasEntities", context.serialize(session.getAccelerometerMeasEntities()));
+            return object;
+        };
+
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(GyroscopeMeasEntity.class, gyroEntitySerializer)
+                .registerTypeAdapter(AccelerometerMeasEntity.class, accelEntitySerializer)
+                .registerTypeAdapter(SessionWithMeasurements.class, sessionEntitySerializer)
+                .create();
     }
 
-    public String serialize() {
-        return this.gson.toJson(this.sessionData);
+    public String serialize() { return this.gson.toJson(this.sessionData); }
+
+    /**
+     * The following function iterates over all attributes of a child
+     * object and adds them to the parent object.
+     */
+    private JsonElement addToParent(JsonObject parent, JsonObject child) {
+        Set<Map.Entry<String, JsonElement>> entries = child.entrySet();
+        for (Map.Entry<String, JsonElement> entry : entries) {
+            parent.add(entry.getKey(), entry.getValue());
+        }
+        return parent;
     }
 }
