9497595a8c2d70f89281dab15a44a467107d9c9b
krp97 <k.plata576@gmail.com>
2021-06-06 00:56:35 +0200

    Scan and pick first MetaWear device nearby

--- app/src/main/java/pl/ppiwd/exerciseanalyst/activities/utils/BluetoothDeviceScanner.java
+++ app/src/main/java/pl/ppiwd/exerciseanalyst/activities/utils/BluetoothDeviceScanner.java
@@ -1,40 +1,46 @@
 package pl.ppiwd.exerciseanalyst.activities.utils;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.Handler;
 import android.util.Log;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.util.ArrayList;
-
 import pl.ppiwd.exerciseanalyst.utils.Callback;
 
 public class BluetoothDeviceScanner {
     private BluetoothAdapter blAdapter;
     private Context appContext;
     private Callback<String> deviceFoundCallback;
+    Boolean cbFired = false;
 
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            Log.i("BluetoothDeviceScanner", "received broadcast message");
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String name = device.getName();
-                String mac = device.getAddress();
-                Log.i("BluetoothDeviceScanner", "Detected device\n name: "
-                        + name + "\n MAC: " + mac);
-                if (device.getName().equals("MetaWear")) {
-                    deviceFoundCallback.run(mac);
+            Log.i("BluetoothDeviceScanner", "received broadcast message" + action);
+            switch (action) {
+                case BluetoothDevice.ACTION_FOUND: {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String name = device.getName();
+                    String mac = device.getAddress();
+                    Log.i("BluetoothDeviceScanner", "Detected device\n name: "
+                            + name + "\n MAC: " + mac);
+
+                    // This bit is slightly hacky, but we want to auto-connect
+                    // to the first MetaWear device that we find.
+                    if (device.getName().equals("MetaWear")) {
+                        cbFired = true;
+                        deviceFoundCallback.run(mac);
+                        BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    }
+                    break;
+                }
+                case BluetoothAdapter.ACTION_DISCOVERY_FINISHED: {
+                    if (!cbFired) {
+                        deviceFoundCallback.run(null);
+                    }
+                    break;
                 }
             }
         }
@@ -45,11 +51,15 @@ public class BluetoothDeviceScanner {
         deviceFoundCallback = deviceFound;
         blAdapter = BluetoothAdapter.getDefaultAdapter();
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
         appContext.registerReceiver(receiver, filter);
+        blAdapter.startDiscovery();
         Log.i("BluetoothDeviceScanner", "starting discovery");
     }
 
     public void close() {
+        Log.i("BluetoothDeviceScanner", "close()");
         appContext.unregisterReceiver(receiver);
         blAdapter.cancelDiscovery();
     }
