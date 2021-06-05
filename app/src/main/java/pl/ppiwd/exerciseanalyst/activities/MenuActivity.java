8488015a02cfe27b502e571905709ccacaecc88e
krp97 <k.plata576@gmail.com>
2021-06-06 00:06:46 +0200

    Show progress bar while scanning for MetaWear

--- app/src/main/java/pl/ppiwd/exerciseanalyst/activities/MenuActivity.java
+++ app/src/main/java/pl/ppiwd/exerciseanalyst/activities/MenuActivity.java
@@ -1,9 +1,12 @@
 package pl.ppiwd.exerciseanalyst.activities;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,12 +15,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import pl.ppiwd.exerciseanalyst.R;
+import pl.ppiwd.exerciseanalyst.activities.utils.BluetoothDeviceScanner;
 import pl.ppiwd.exerciseanalyst.common.BroadcastMsgs;
 import pl.ppiwd.exerciseanalyst.common.Constants;
 import pl.ppiwd.exerciseanalyst.utils.WipeDatabase;
@@ -25,36 +34,57 @@ import pl.ppiwd.exerciseanalyst.utils.WipeDatabase;
 public class MenuActivity extends AppCompatActivity {
 
     private Button gatherDataButton;
-    private Button wipeDbButton;
     private Button openWebViewButton;
     private boolean isDebugActive;
     private Button startTrainingButton;
+    private CardView cardViewSessionDetails;
+    private CardView cardViewDeviceDetails;
+    private TextView deviceName;
+    private TextView deviceMacUi;
+    private TextView deviceStatus;
+    private ProgressBar pbDeviceScan;
+    private BluetoothDeviceScanner blHandler;
+    private String deviceMac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.isDebugActive = false;
         super.onCreate(savedInstanceState);
-        Log.i("DataCollectionActivity()", "onCreate()");
+        Log.i("MenuActivity", "onCreate()");
         setContentView(R.layout.activity_main);
         initViews();
         init();
     }
 
     private void init() {
-        Log.i("DataCollectionActivity", "init()");
+        Log.i("MenuActivity", "init()");
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BroadcastMsgs.METAMOTION_SERVICE_ALIVE_RESP);
         intentFilter.addAction(BroadcastMsgs.ACTIVITY_BROADCAST_INTENT_ACTION);
-
     }
 
     private void initViews() {
         gatherDataButton = findViewById(R.id.btn_start_meta_motion);
         openWebViewButton = findViewById(R.id.btn_open_webview);
         startTrainingButton = findViewById(R.id.btn_start_training);
+        deviceName = findViewById(R.id.tv_device_name);
+        deviceMacUi = findViewById(R.id.tv_mac_address);
+        deviceStatus = findViewById(R.id.tv_device_status);
+        cardViewDeviceDetails = findViewById(R.id.cv_device_status);
+        cardViewSessionDetails = findViewById(R.id.cv_tagging_session);
+        pbDeviceScan = findViewById(R.id.pb_scan_mode);
+
         startTrainingButton.setOnClickListener(view -> openTrainingView(Constants.TRAINING_REGULAR));
         gatherDataButton.setOnClickListener(view -> openTrainingView(Constants.TRAINING_DATA_GATHERING));
         openWebViewButton.setOnClickListener(view -> openWebViewStats());
+
+
+        cardViewSessionDetails.setVisibility(View.GONE);
+        cardViewDeviceDetails.setOnClickListener(view -> initiateBlScanning());
+        deviceMacUi.setVisibility(View.GONE);
+        deviceName.setVisibility(View.GONE);
+        pbDeviceScan.setVisibility(View.GONE);
+        deviceStatus.setText("Tap to scan for device");
     }
 
     private void openTrainingView(String serviceUrl) {
@@ -102,11 +132,9 @@ public class MenuActivity extends AppCompatActivity {
             isDebugActive = !isDebugActive;
             item.setChecked(isDebugActive);
             if (isDebugActive) {
-                wipeDbButton.setVisibility(View.VISIBLE);
                 gatherDataButton.setVisibility(View.VISIBLE);
                 Toast.makeText(this, "Debug mode: enabled", Toast.LENGTH_LONG).show();
             } else {
-                wipeDbButton.setVisibility(View.GONE);
                 gatherDataButton.setVisibility(View.GONE);
                 Toast.makeText(this, "Debug mode: disabled", Toast.LENGTH_LONG).show();
             }
@@ -123,4 +151,34 @@ public class MenuActivity extends AppCompatActivity {
         finish();
     }
 
+    private void initiateBlScanning() {
+        Log.i("MenuActivity", "initiateBlScanning()");
+        deviceStatus.setText("Scanning for devices");
+        pbDeviceScan.setVisibility(View.VISIBLE);
+        int hasLocationPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 2);
+        }
+
+        blHandler = new BluetoothDeviceScanner(this, (deviceMac) -> {
+           this.deviceMac = deviceMac;
+           showDeviceStats("MetaWear", deviceMac);
+        });
+    }
+
+    private void showDeviceStats(String name, String mac) {
+        this.deviceStatus.setText("Wearable available");
+        this.deviceName.setText(name);
+        this.deviceName.setVisibility(View.VISIBLE);
+        this.deviceMacUi.setText(mac);
+        this.deviceName.setVisibility(View.VISIBLE);
+    }
 }
